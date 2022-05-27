264366e089af7fa81fc17cdad8e5dc5c43bd8017
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-27 20:26:29 -0300

    json/module insertion

--- utils/modules.js
+++ utils/modules.js
@@ -1,22 +1,22 @@
 export const songs = [
-    {"name": "Wild Nothing - Shadow", "link": "https://www.youtube.com/watch?v=eeb262NT7S0", "lyrics": "https://genius.com/Wild-nothing-shadow-lyrics", "album": "https://m.media-amazon.com/images/I/81+4qvYc0nL._AC_SX425_.jpg", 
+    {"name": "Wild Nothing - Shadow", "link": "https://www.youtube.com/watch?v=eeb262NT7S0", "lyrics": "https://genius.com/Wild-nothing-shadow-lyrics", "album": "https://i1.sndcdn.com/artworks-000024979953-5ibw8m-t500x500.jpg", 
     "phrase": `Bem, espere bastante músicas dessa banda (Posso falar mais tarde sobre minha história com ela, inclusive comecei a escutá-la poucos dias antes da gente conversar pela primeira vez.) Essa playlist em geral vai 
     seguir uma cronologia parecida com a da sua, sim estou copiando na cara dura. Essa música retrata a essência de quem eu era em 2018, uma visão introspectiva minha que hoje em dia brincamos mas foi o momento em que me senti 
     mais vulnerável em todos os sentidos. Essa música, tem um tom cético né? e a primeira frase "Oh, why is your hate so addicting and I, well I wonder where you've been, I don't see you often me faz lembrar sobre essa época 
     em que você me odiava (com todas as razões) e meu objetivo diante de tudo aquilo, era simplesmente te mostrar que eu não era babaca e que eu estava ferido. Mal sabia que você seria minha "Luz". `}
-,   
+    ,   
     {"name": "Wild Nothing - Letting Go", "link": "https://www.youtube.com/watch?v=05iNw6LFs10", "lyrics": "https://genius.com/Wild-nothing-letting-go-lyrics",
-    "album": "https://monkeybuzz.com.br/wp-content/uploads/2018/09/wild-nothing-indigo-5b90f7a4a0341-1024x1024.jpg", "phrase": `Falando do começo da nossa amizade propriamente dito, essa música foi a primeira música 
-    do Wild Nothing que escutei, e como você sabe, sou louco por essa banda até hoje. Letting Go, eu tentava sarar aquela dor, você me ajudou muito, muito. "Surreal, the way you make me out, the way you crash me down".
+    "album": "https://t2.genius.com/unsafe/327x327/https%3A%2F%2Fimages.genius.com%2F4f0a45648128e3444f0746d0b6142798.1000x1000x1.jpg", "phrase": `Falando do começo da nossa amizade propriamente dito, essa música foi a primeira música 
+     do Wild Nothing que escutei, e como você sabe, sou louco por essa banda até hoje. Letting Go, eu tentava sarar aquela dor, você me ajudou muito, muito. "Surreal, the way you make me out, the way you crash me down".
      Fazia muito tempo em que eu não tinha uma conversa verdadeiramente boa com alguém. Sim eu confesso, me abri rápido demais com você, mas não me arrependo. Como você disse (ou escreveu?) na "cartinha", a única coisa 
      que você fazia nessa época era me ouvir e responder, e isso já significava muito para mim. "I step inside", me lembro como se fosse ontem eu adentrando na sua sala, tremendo e com calafrios me perguntando se você 
      realmente me ajudaria. Quem diria que sua ajuda seria muito além do que um simples trabalho de neologia em português.` }
-,
+    ,
 
     {"name": "Mac DeMarco - For the first time", "link": "https://www.youtube.com/watch?v=cZCm_i6YvAk", "lyrics": "https://genius.com/Mac-demarco-for-the-first-time-lyrics",
     "album": "https://t2.genius.com/unsafe/327x327/https%3A%2F%2Fimages.genius.com%2Fe016d633816ada4c55a26832da91f2f4.1000x1000x1.jpg", "phrase": `Essa música você já sabe o motivo, só faco questão de colocá-la aqui porque o sentimento é fidedigno. Ficamos um tempinho sem 
     conversar, mas em novembro do ano passado quando a gente voltou a se falar, realmente pareceu que nada tinha mudado. Eu sei lá fe, é atemporal, são 4 anos de amizade mas p mim sinceramente parece que todo dia te conheço de novo, sempre to te conhecendo mais e mais, 
-e essa magia nunca vai acabar. Você <strong> sempre </strong> me impressiona, e todo dia com você é um dia novo, e sou grato de poder construir esse relacionamento tão lindo com você todos os dias, muito obrigado 
+    e essa magia nunca vai acabar. Você <strong> sempre </strong> me impressiona, e todo dia com você é um dia novo, e sou grato de poder construir esse relacionamento tão lindo com você todos os dias, muito obrigado 
     `},
 
     {"name": "Wild Nothing - Only Heather", "link": "https://www.youtube.com/watch?v=Ghb-u7u36B8", "lyrics": "https://genius.com/Wild-nothing-only-heather-lyrics", 
@@ -32,10 +32,30 @@ e essa magia nunca vai acabar. Você <strong> sempre </strong> me impressiona, e
     ,
     {"name": "Wild Nothing - Oscillation", "link": "https://www.youtube.com/watch?v=8TZcKjL-J3A", "lyrics": "https://genius.com/Wild-nothing-oscillation-lyrics", "album": "https://monkeybuzz.com.br/wp-content/uploads/2018/09/wild-nothing-indigo-5b90f7a4a0341-1024x1024.jpg", 
     "phrase": `Você também é meu trevo de quatro folhas, eu te amo, eu te amo.`}
-    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
-    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
-    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
-    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
-    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
+    ,
+    {"name": "The Smiths - There is a Light Never Goes out", "link": "https://www.youtube.com/watch?v=3r-qDvD3F3c", "lyrics": "https://genius.com/The-smiths-there-is-a-light-that-never-goes-out-lyrics", "album": "http://s2.glbimg.com/QdN-4MPwUDhKjFS-WjjQ9Xjio18=/smart/e.glbimg.com/og/ed/f/original/2017/07/20/cd_deluxe_top.jpg", 
+    "phrase": `The Smiths? Muito clichê e mainstream né? Sinto que ouvir essa música e pensar em você é como eu aceitando que eu sou meloso e apaixonadinho por você e não tenho medo desse título porque esse sentimento é tão bom, que ouvir essa música é a mesma coisa. 
+    "Take me anywhere, I dont't care, I don't care" sua presença é tudo p mim, seja em call, seja fisicamente, faz comigo qualquer coisa, vamos fazer uma call só p ficar um olhando p cara do outro com sorriso bobo e coração mole, vamos no mercado, vamos cozinhar, vamos cantar... Se
+    tem um luz que nunca se vai, é meu amor por você.`}
+    ,
+    {"name": "Wild Nothing - Our Composition Book", "link": "https://www.youtube.com/watch?v=wKAzoGh6B7E", "lyrics": "https://genius.com/Wild-nothing-our-composition-book-lyrics", "album": "https://m.media-amazon.com/images/I/81jo0qLHcuL._AC_SL1500_.jpg", "phrase": `Não sei se você se lembra, mas quando você veio aqui em casa pela segunda
+    vez, coloquei essa música p tocar, ela é do tipo de música que só me faz pensar sobre esses nossos momentos, nossas coisinhas, nossas brincadeiras, essa coisinha nossa inexplicável... que só nós dois sabemos sabe? Dá borboletas só de pensar... `}
+    ,
+    {"name" : "The Stylistics - You Make Me Feel Brand New", "link": "https://www.youtube.com/watch?v=dJm51jsRoo0", "lyrics": "https://genius.com/The-stylistics-you-make-me-feel-brand-new-lyrics", "album": "https://i.scdn.co/image/ab67616d0000b273d2a94a6a7b7df7339cf9b145", 
+    "phrase": `Uma música de quase 50 anos? Vou explicar... Tenho lembranças de quando eu tinha uns 5 ou 4 anos vendo minha mãe cantar essa música p meu pai quando passava na rádio, não entendia a letra então tanto faz. A verdadeira história na verdade não é da minha mãe com meu pai, mas sim com meu avô e avó paternos.
+    Como já te disse, minha avó faleceu antes mesmo de eu nascer, na verdade nem minha mãe a conheceu. Eu perguntei um pouco sobre a minha avó p ele, já que sei pouquíssimas coisas, ele falou que  escutava essa música quando eles se conhecerem na adolescência, e que toda vez que escutava, se emocionava. Ele me mostrou a música,
+    que como já falei já "conhecia" e ele aos prantos falou: "Vini, quando você conhecer a pessoa certa, vai lembrar dessa música", não foi diferente. A letra, puts, eu to escrevendo isso aqui chorando... Ignora o fato de que ela tem quase 50 anos e olha a letra... enfim, to chorando e não consigo pensar em escrever mais nada.
+    `}
+    ,
+    {"name": "DIIV - Dopamine", "link": "https://www.youtube.com/watch?v=HdemOVIXuAI", "lyrics": "https://genius.com/Diiv-dopamine-lyrics", "album": "https://m.media-amazon.com/images/I/91dpGvabz3L._AC_SL1400_.jpg", "phrase": `A alegria fodida de estar ao seu lado é melhor que qualquer coisa, 
+    dopamina dopamina "You're my sun and I'm your cloud", "Got so high I finnaly felt like myself" Estar com você é simplesmente a melhor coisa da minha vida.`}
+    ,
+    {"name": "girl in red - we fell in love in october", "link": "https://www.youtube.com/watch?v=iggmiF7DNoM", "lyrics": "https://genius.com/Girl-in-red-we-fell-in-love-in-october-lyrics", "album": "https://m.media-amazon.com/images/I/712MoKaNeZL._SL1200_.jpg", "phrase": `Nada melhor que 
+    pensar na música de uma cantora que você me recomendou no começo de 2019... Auto-explicativo né? Não nos apaixonamos em outubro, mas You'll be my girl my girl my girl, you'll be my world my world my world`}
+    ,
+    {"name": "Wild Nothing - Through Windows", "link": "https://www.youtube.com/watch?v=uDbJiCHE0yA", "lyrics": "https://genius.com/Wild-nothing-through-windows-lyrics", "album": "https://monkeybuzz.com.br/wp-content/uploads/2018/09/wild-nothing-indigo-5b90f7a4a0341-1024x1024.jpg", "phrase": `
+    Me apaixono por você todos os dias, todos os dias eu me boiolo mais ainda por você, a sensação é que o amor se multiplica a cada segundo, é muito amor, eu sinto muita coisa e obviamente, como você disse, três ou um milhão de palavras não conseguiriam explicar o que eu sinto, mas posso te dizer que
+    "This love is all yours, now and forever, now and forever".
+    `}
 
 ];
