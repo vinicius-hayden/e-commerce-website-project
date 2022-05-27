0f0ab1c7565f0ad13499a4c28591c9b28534697a
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-27 14:16:00 -0300

    json/module insertion

--- utils/modules.js
+++ utils/modules.js
@@ -11,5 +11,31 @@ export const songs = [
      Fazia muito tempo em que eu não tinha uma conversa verdadeiramente boa com alguém. Sim eu confesso, me abri rápido demais com você, mas não me arrependo. Como você disse (ou escreveu?) na "cartinha", a única coisa 
      que você fazia nessa época era me ouvir e responder, e isso já significava muito para mim. "I step inside", me lembro como se fosse ontem eu adentrando na sua sala, tremendo e com calafrios me perguntando se você 
      realmente me ajudaria. Quem diria que sua ajuda seria muito além do que um simples trabalho de neologia em português.` }
+,
+
+    {"name": "Mac DeMarco - For the first time", "link": "https://www.youtube.com/watch?v=cZCm_i6YvAk", "lyrics": "https://genius.com/Mac-demarco-for-the-first-time-lyrics",
+    "album": "https://t2.genius.com/unsafe/327x327/https%3A%2F%2Fimages.genius.com%2Fe016d633816ada4c55a26832da91f2f4.1000x1000x1.jpg", "phrase": `Essa música você já sabe o motivo, só faco questão de colocá-la aqui porque o sentimento é fidedigno. Ficamos um tempinho sem 
+    conversar, mas em novembro do ano passado quando a gente voltou a se falar, realmente pareceu que nada tinha mudado. Eu sei lá fe, é atemporal, são 4 anos de amizade mas p mim sinceramente parece que todo dia te conheço de novo, sempre to te conhecendo mais e mais, 
+e essa magia nunca vai acabar. Você <strong> sempre </strong> me impressiona, e todo dia com você é um dia novo, e sou grato de poder construir esse relacionamento tão lindo com você todos os dias, muito obrigado 
+    `},
+
+    {"name": "Wild Nothing - Only Heather", "link": "https://www.youtube.com/watch?v=Ghb-u7u36B8", "lyrics": "https://genius.com/Wild-nothing-only-heather-lyrics", 
+    "album": "https://m.media-amazon.com/images/I/81+4qvYc0nL._AC_SX425_.jpg", "phrase" : `A música que escutei no percurso de volta p casa no melhor dia da minha vida, 3 de março. Eu nunca havia sentido tantas borboletas em meu estômago.
+    Essa música me lembra inteiramente você e esse sentimento intenso e gostosinho de se apaixonar por você todos os dias, nada mudou desde aquele dia, eu continuo me apaixonando mais e mais por você. "She has something that I've never known", apesar 
+    de modéstia à parte te conhecer muito bem, você sempre teve algo que me fascinou, talvez seu senso de humor, talvez seu jeito de ser, "Couldn't explain it, wouldn't even try, she's so lovely she makes me feel high", é gato, você me deixa sem palavras.`}
+    ,
+    {"name": "松原 みき - 真夜中のドア/Stay with me", "link": "https://www.youtube.com/watch?v=VEe_yIbW64w", "lyrics": "https://genius.com/Genius-romanizations-miki-matsubara-mayonaka-no-door-stay-with-me-romanized-lyrics"
+    , "album": "https://pbs.twimg.com/media/ErmWxfqXYAE4kSN.jpg", "phrase": `Essa música, sei lá, me faz lembrar todas as vezes que nos despedimos, desde o dia em que você veio aqui em casa e a gente te deu carona e no meio do percurso porque eu sou maluco comecei a procurar as músicas de citypop que se pareciam
+    com a música do The Weeknd, você se lembra? Da despedida de quando você foi p aeroporto (Me arrependo de não ter te abraçado forte), ou um simples tchau nas nossas calls. Ouvi essa música por acaso, e toda vez agora me lembro da leve dor de se despedir de você, Stay with me, eu viveria e morreria com você, nunca me enjoaria, 
+    e compactuo agora mais do que nunca com o que você disse que o tempo é suficiente porque é com você e insuficiente porque é com você. 
+    Stay with me, stay with me.`}
+    ,
+    {"name": "Wild Nothing - Oscillation", "link": "https://www.youtube.com/watch?v=8TZcKjL-J3A", "lyrics": "https://genius.com/Wild-nothing-oscillation-lyrics", "album": "https://monkeybuzz.com.br/wp-content/uploads/2018/09/wild-nothing-indigo-5b90f7a4a0341-1024x1024.jpg", 
+    "phrase": `Você também é meu trevo de quatro folhas, eu te amo, eu te amo.`}
+    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
+    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
+    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
+    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
+    // {"name": "", "link": "", "lyrics": "", "album": "", "phrase": ``}
 
 ];
