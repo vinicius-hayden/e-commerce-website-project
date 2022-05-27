98427cbf1f0fb1f6895e44c329318a5f4f16fa42
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-26 21:54:44 -0300

    modules.js

---
+++ utils/modules.js
@@ -0,0 +1,15 @@
+export const songs = [
+    {"name": "Wild Nothing - Shadow", "link": "https://www.youtube.com/watch?v=eeb262NT7S0", "lyrics": "https://genius.com/Wild-nothing-shadow-lyrics", "album": "https://m.media-amazon.com/images/I/81+4qvYc0nL._AC_SX425_.jpg", 
+    "phrase": `Bem, espere bastante músicas dessa banda (Posso falar mais tarde sobre minha história com ela, inclusive comecei a escutá-la poucos dias antes da gente conversar pela primeira vez.) Essa playlist em geral vai 
+    seguir uma cronologia parecida com a da sua, sim estou copiando na cara dura. Essa música retrata a essência de quem eu era em 2018, uma visão introspectiva minha que hoje em dia brincamos mas foi o momento em que me senti 
+    mais vulnerável em todos os sentidos. Essa música, tem um tom cético né? e a primeira frase "Oh, why is your hate so addicting and I, well I wonder where you've been, I don't see you often me faz lembrar sobre essa época 
+    em que você me odiava (com todas as razões) e meu objetivo diante de tudo aquilo, era simplesmente te mostrar que eu não era babaca e que eu estava ferido. Mal sabia que você seria minha "Luz". `}
+,   
+    {"name": "Wild Nothing - Letting Go", "link": "https://www.youtube.com/watch?v=05iNw6LFs10", "lyrics": "https://genius.com/Wild-nothing-letting-go-lyrics",
+    "album": "https://monkeybuzz.com.br/wp-content/uploads/2018/09/wild-nothing-indigo-5b90f7a4a0341-1024x1024.jpg", "phrase": `Falando do começo da nossa amizade propriamente dito, essa música foi a primeira música 
+    do Wild Nothing que escutei, e como você sabe, sou louco por essa banda até hoje. Letting Go, eu tentava sarar aquela dor, você me ajudou muito, muito. "Surreal, the way you make me out, the way you crash me down".
+     Fazia muito tempo em que eu não tinha uma conversa verdadeiramente boa com alguém. Sim eu confesso, me abri rápido demais com você, mas não me arrependo. Como você disse (ou escreveu?) na "cartinha", a única coisa 
+     que você fazia nessa época era me ouvir e responder, e isso já significava muito para mim. "I step inside", me lembro como se fosse ontem eu adentrando na sua sala, tremendo e com calafrios me perguntando se você 
+     realmente me ajudaria. Quem diria que sua ajuda seria muito além do que um simples trabalho de neologia em português.` }
+
+];
